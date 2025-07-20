4a9ff5df287917bbc18e86dccf060f38243f1335
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2025-07-20 10:20:55 +0530

    added css

--- firstself/frontend/src/App.jsx
+++ firstself/frontend/src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom"
+import Favs from './pages/Favs';
+import NavBar from './components/NavBar';
+
 function App() {
 
   return (
-    <>
-    <Home/>
-    </>
+    <div>
+      <NavBar />
+
+      
+      {/* The NavBar component is imported and used here to display the navigation links */}
+      {/* The Routes component is used to define the different routes in the application */}
+      {/* Each Route component defines a path and the component to render for that path */}
+
+
+      <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favs />} />
+        
+        
+      </Routes>
+      
+      </main>
+    </div>
   )
 }
 
---
+++ firstself/frontend/src/pages/Favs.jsx
@@ -0,0 +1,11 @@
+function Favourite() {
+    return (
+        <>
+            <div className="favourites-empty">
+                <h2>No Favourites Yet</h2>
+                <p>Start adding some movies to your favourites!</p>
+            </div>
+        </>
+    )
+}
+export default Favourite;
