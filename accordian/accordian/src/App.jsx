7bd6ba8f2da40bf74bbc98a07f95e193724f1c0c
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2025-07-22 22:54:22 +0530

    accordian-secondproject

---
+++ accordian/accordian/src/App.jsx
@@ -0,0 +1,21 @@
+ 
+import Accordian from './components/index.jsx/index.jsx'
+// import data from './components/index.jsx/data.js'
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+      <div className="App">
+        <Accordian/>
+      </div>
+      
+    
+ 
+    </>
+  )
+}
+
+export default App
