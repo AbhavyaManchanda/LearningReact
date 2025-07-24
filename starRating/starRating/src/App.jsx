c953ab03a7c545986ab0635dfc68c10b2d909f83
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2025-07-24 23:55:13 +0530

    star-rating

---
+++ starRating/starRating/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import StarRating from './components'
+import './App.css'
+
+function App() {
+   
+
+  return (
+    <>
+      <StarRating noOfStars={5} />
+    </>
+  )
+}
+
+export default App
