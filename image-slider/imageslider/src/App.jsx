04dd75716e8a8cbf9ec8c9bf01dc06b05ff2d9bb
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2025-07-26 23:41:44 +0530

    image-slider

---
+++ image-slider/imageslider/src/App.jsx
@@ -0,0 +1,14 @@
+import ImageSlider from './components'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} />
+        
+    </>
+  )
+}
+
+export default App
