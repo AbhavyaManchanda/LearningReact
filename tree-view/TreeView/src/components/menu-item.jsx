4a46e19ed2a6dbaf06afb5c6bd5ced09a610e313
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2025-07-28 15:40:02 +0530

    done without comments

--- tree-view/TreeView/src/components/menu-item.jsx
+++ tree-view/TreeView/src/components/menu-item.jsx
@@ -3,38 +3,46 @@ import MenuList from "./menu-list";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
 export default function MenuItem({ item }) {
-  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
-  function handleToggleChildren(getCurrentlabel) {
-    setDisplayCurrentChildren({
-      ...displayCurrentChildren,
-      [getCurrentlabel]: !displayCurrentChildren[getCurrentlabel],
-    });
-  }
+    function handleToggleChildren(getCurrentlabel) {
+        setDisplayCurrentChildren({
+        ...displayCurrentChildren,
+        [getCurrentlabel]: !displayCurrentChildren[getCurrentlabel],
+        });
+    }
 
-  console.log(displayCurrentChildren);
+    console.log(displayCurrentChildren);
 
-  return (
+    return (
     <li>
-      <div className="menu-item">
+    <div className="menu-item">
         <p>{item.label}</p>
+        
         {item && item.children && item.children.length ? (
-          <span onClick={() => handleToggleChildren(item.label)}>
+        
+            <span onClick={() => handleToggleChildren(item.label)}>
+                    
             {displayCurrentChildren[item.label] ? (
-              <FaMinus color="#fff" size={25} />
+            <FaMinus color="#fff" size={25} />
             ) : (
-              <FaPlus color="#fff" size={25} />
+            <FaPlus color="#fff" size={25} />
             )}
-          </span>
-        ) : null}
-      </div>
+            </span>
+                
+            ) : null}
+              {/* this is the menu item with children */}
+    </div>
 
-      {item &&
-      item.children &&
-      item.children.length > 0 &&
-      displayCurrentChildren[item.label] ? (
-        <MenuList list={item.children} />
-      ) : null}
-    </li>
-  );
+    {item &&
+    item.children &&
+    item.children.length > 0 && displayCurrentChildren[item.label]
+        ? ( <MenuList list={item.children} /> )
+                : null}
+            
+        
+        </li>
+        //in this we are checking if the item has children and if the children are displayed . and then we are rendering the menu list component with the children of the current item depending upon the state of the children of the current item.
+        
+    );
 }
